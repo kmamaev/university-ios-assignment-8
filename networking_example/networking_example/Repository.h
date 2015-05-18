bbdd4511efc43ee673173ec2d45c37e2b06e5f2c
Konstantin Mamaev <konstantin.mamaev@noveogroup.com>
2015-05-18 18:55:18 +0600

    Extend model of repository.

--- networking_example/networking_example/Repository.h
+++ networking_example/networking_example/Repository.h
@@ -2,7 +2,15 @@
 
 
 @interface Repository : NSObject
-@property (copy, nonatomic) NSString *name;
-@property (copy, nonatomic) NSString *fullName;
-@property (assign, nonatomic) NSInteger commitsCount;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *fullName;
+@property (nonatomic, assign) NSInteger commitsCount;
+@property (nonatomic, copy) NSObject *owner;
+@property (nonatomic, copy) NSString *repositoryDescription;
+@property (nonatomic, assign) BOOL isPrivate;
+@property (nonatomic, strong) NSURL *repositoryUrl;
+@property (nonatomic, strong) NSURL *cloneUrl;
+@property (nonatomic, assign) NSInteger forksCount;
+@property (nonatomic, strong) NSDate *creationDate;
+@property (nonatomic, strong) NSDate *lastCommitDate;
 @end
